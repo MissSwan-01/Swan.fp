9f4b693b2f7bb2c0be10c1f29e1397b4230ad2bf
Ms.Swan <hyrulezelda504@gmail.com>
2026-05-22 00:40:14 +0530

    Update run-map.js

--- run-map.js
+++ run-map.js
@@ -1,23 +1,23 @@
 module.exports = {
 
   // ================= RUN 1 =================
-  /*1: [
+  1: [
     { tierId: 10, startPage: 1,   endPage: 7 },
     { tierId: 10, startPage: 8,   endPage: 14 },
     { tierId: 10, startPage: 15,  endPage: 21 },
     { tierId: 10, startPage: 22,  endPage: 28 },
     { tierId: 10, startPage: 29,  endPage: 35 },
     { tierId: 10, startPage: 36,  endPage: 42 }
-  ], */
+  ],
 
-  1: [
+  /*1: [
     { tierId: 1, startPage: 1,   endPage: 100 },
     { tierId: 1, startPage: 101,   endPage: 200 },
     { tierId: 1, startPage: 201,  endPage: 300 },
     { tierId: 1, startPage: 301,  endPage: 400 },
     { tierId: 1, startPage: 401,  endPage: 500 },
     { tierId: 1, startPage: 501,  endPage: 600 }
-  ], 
+  ], */
 
   // ================= RUN 2 =================
   2: [
