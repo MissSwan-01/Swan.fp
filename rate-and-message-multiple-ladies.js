1bbed4480fc5cc3619dc355a26689780cd3a61d3
Ms.Swan <hyrulezelda504@gmail.com>
2026-07-12 14:26:27 +0530

    Update rate-and-message-multiple-ladies.js

--- rate-and-message-multiple-ladies.js
+++ rate-and-message-multiple-ladies.js
@@ -22,12 +22,13 @@ module.exports = async function runRateAndMessageMultipleLadies(page, tierConfig
     'Elowen','Lady','V I R G O','LionLady','marym','Alexia','Devi','Aicha','Riley','Pina','Safa','WISSAM','Kathryn','Bayonetta','Gracyn','Cemi25','Eva','Abbatia','Quinz','nana__osaki','ᏔᎯ ᏞᎥᎠ','jas','زوزو','MALOKA','Ania','Victors Secret',
     'laura','Ella','Jurria','Gloria','welle','Radha','Rosalina','Funny Face','reyhan','Yuna','Isabel','steph','maryann','Lovlee','Lana','-𝐓𝐫𝐢𝐤𝐢𝐳𝐳𝐙-','Yeluna Corazón','x_.sono.prvt_','sabrina13','Jade','VenusDoom','Mikel','Singin-Suzun','Lina lyn',
     'Michel','אלכס','Uruguaya','Jaisita','X_Haiko_X','Saddalyn','sa.ahil','MintyRose','kaya21','baraa bf','mnijeonhaneen','TheResa75','Méllisa','Lady Abbie Queen','Abc','ساجده','Sharisse','...','Pamuk','Maya','R H A E N Y R A','Sami',
+    'katrina','QUEEN OF MEAN','alain dz',
     // add all names you want to exclude
   ].map(n => n.toLowerCase())); // convert to lowercase for case-insensitive matching
 
-  const m1 = 'Sending positive vibes! 🌿🍒 Have a cherry-fic day my friend! (｡♥‿♥｡)';
-  const m2 = 'Sending positive vibes! 🌿🍒 Have a cherry-fic day my friend! (｡♥‿♥｡)'; //already won
-  const m3 = 'Sending positive vibes! 🌿🍒 Have a cherry-fic day my friend! (｡♥‿♥｡)'; //168
+  const m1 = 'Happy Sunday my friend 😽 You are looking meow-velous! ฅ≽^•⩊•^≼ฅ';
+  const m2 = 'Happy Sunday my friend 😽 You are looking meow-velous! ฅ≽^•⩊•^≼ฅ'; //already won
+  const m3 = 'Happy Sunday my friend 😽 You are looking meow-velous! ฅ≽^•⩊•^≼ฅ'; //168
 
   const tabLabel = page._guid || 'T?'; //internal tab label in playwright
 
